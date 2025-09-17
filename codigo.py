fc66f38291575b8cbf9f5a1b20f51d5165f5ef76
gabriel <gabrielborges0111@gmail.com>
2025-09-16 21:53:07 -0300

    16/09

--- codigo.py
+++ codigo.py
@@ -1,11 +1,66 @@
-lista_compras = []
-for lista in range(1, 10):
-    user = str(input("adicione oq vc quer na lista:"))
-    if user == ("sair"):
-        break
+# numeros = [1, 2, 2, 3, 4, 4,5, 1]
+# unicos = []
+
+# for i in numeros:
+#     if i not in unicos:
+#         unicos.append(i)
 
 
-lista_compras = [user]
-response = lista_compras 
+# print(unicos)
 
-print(response)
+
+# numeros = [1, 2, 2, 3, 4, 4,5, 1]
+# unicos = list(set(numeros))
+# print(unicos)
+
+#######################################################################
+
+
+# lista1 = ["mirella", "gabriel", "chico"]
+# lista2 = ["mada", "clau", "chico"]
+
+# repets = set(lista1)
+# repets2 = set(lista2)
+
+# interseçao = repets & repets2
+
+# interseçao2 = list(interseçao)
+
+# print(f"esse é o nome repetido: {interseçao2}")
+
+
+###########################################################
+
+
+# lista1 = ["mirella", "gabriel", "chico"]
+# lista2 = ["mada", "clau", "chico"]
+
+# conjunto1 = set(lista1)
+# conjunto2 = set(lista2)
+
+
+
+# diferença = conjunto1 - conjunto2
+# print("diferença", diferença)
+
+
+
+
+############################################################
+
+
+frase = input("digite uma frase:")
+
+palavras = frase.split()
+
+unicas = []
+
+for palavra in palavras:
+    if palavra not in unicas:
+        unicas.append(palavras)
+print("palavras unicas:")
+
+for palavra in unicas:
+            print(palavra)
+
+
--- src/app.py
+++ src/app.py
@@ -1,320 +1,342 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
+# from flask import Flask
+# from dotenv import load_dotenv
+# import os
 
 
-load_dotenv()
+# load_dotenv()
 
 
-app = Flask (__name__)
+# app = Flask (__name__)
 
 
-@app.route("/")
-def home():
-    return"Olá, mundo!"
+# @app.route("/")
+# def home():
+#     return"Olá, mundo!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 
 
   
 
-# exec 1
-input("Descubra os resultados das contas com a senha de 123:  ")
-n1 = 15
-n2 = 27
-soma = n1 + n2
+# # exec 1
+# input("Descubra os resultados das contas com a senha de 123:  ")
+# n1 = 15
+# n2 = 27
+# soma = n1 + n2
 
-print("O resultado é:", soma)
+# print("O resultado é:", soma)
 
-# exec 2
+# # exec 2
 
-n1 = 12
-n2 = 8
-multiplicacao = n1 * n2
-print("O resultado é:", multiplicacao)
+# n1 = 12
+# n2 = 8
+# multiplicacao = n1 * n2
+# print("O resultado é:", multiplicacao)
 
-# exec 3
-n1 = 25
-n2 = 4
-divisao = n1 // n2
-print("O resultado é:", divisao)
+# # exec 3
+# n1 = 25
+# n2 = 4
+# divisao = n1 // n2
+# print("O resultado é:", divisao)
 
-# exec 4
-n1 = 37
-n2 = 5
-resto = n1 % n2
-print("O resultado é:", resto)
+# # exec 4
+# n1 = 37
+# n2 = 5
+# resto = n1 % n2
+# print("O resultado é:", resto)
 
-# exec 5
-n1 = 3
-n2 = 4
-potencia = n1 ** n2
-print("O resultado é:", potencia)
-
-
-print(10 > 7) # Retorna True pois 10 é maior que 7
-
-print(15 == 20) # Retorna False pois 15 não é igual a 20
-
-print(100 <= 100) # Retorna True pois 100 é igual a 100
-
-print(45 != 30) # Retorna o resultado True é diferente
-
-##################################################################
-
-print("Novo desafio abaixo: ")
-
-print(5 > 2 and 8 > 3)
-
-print(10 < 3 or 4 == 4)
-
-print (not 7 > 2)
-
-print("Novo desafio abaixo: ")
-
-x = 10
-x += 5
-x *= 2
-x -= 4
-print(x)
+# # exec 5
+# n1 = 3
+# n2 = 4
+# potencia = n1 ** n2
+# print("O resultado é:", potencia)
 
 
-a = [1,2,3]
-b = a
-c = [1,2,3]
-print (a is b)
-print (a is c)
+# print(10 > 7) # Retorna True pois 10 é maior que 7
 
- 
-print(""" podemos imprimir
-      multiplas linhas
-      na saida do print""")
+# print(15 == 20) # Retorna False pois 15 não é igual a 20
 
+# print(100 <= 100) # Retorna True pois 100 é igual a 100
 
+# print(45 != 30) # Retorna o resultado True é diferente
 
+# ##################################################################
 
+# print("Novo desafio abaixo: ")
 
-########################################################
-print(f""" 14 . verifique se (a is b): {a is b}
-            14 . verifique se (a is c) : {a is c}""")
+# print(5 > 2 and 8 > 3)
 
+# print(10 < 3 or 4 == 4)
 
-print("o valor é:", a)
-print("o valor de A é:",a, "e o valor de B é:", b)
-print(f"o valor de A é: {a} e o alor de B é: {b}")
+# print (not 7 > 2)
 
-##########################
+# print("Novo desafio abaixo: ")
 
-
-a = [1,2,3]
-b = a
-c = [1,2,3]
-print (a is b)
-print (a is c)
+# x = 10
+# x += 5
+# x *= 2
+# x -= 4
+# print(x)
 
 
-print("o valor é:", )
-print("o valor de A é:",a, "e o valor de B é:", b)
-print(f"o valor de A é: {a} e o alor de B é: {b}")
-
-
-
-numeros = [1,2,3,4,5]
-print(6 in numeros)
-
-
-numeros = [12,15,17,23,27]
-print(17 in numeros)
-
-########################################################################
-
-nome = input("digite seu nome:  ")
-idade = int(input("digite sua idade:  "))
-
-
-Idade_daqui_100 = print (100 - idade)
-
-
-if idade >= 18:  print("vc é maioridade")
-else: print("vc é menor de idade")
-
-
-if 13 <= idade <=19: print("vc é adolescente")
-
-idade += 5 
-print (f"sua idade daqui 5 anos sera de: {idade}")
+# a = [1,2,3]
+# b = a
+# c = [1,2,3]
+# print (a is b)
+# print (a is c)
 
  
-print("vc tem a letra N no nome", 'n' in nome)
+# print(""" podemos imprimir
+#       multiplas linhas
+#       na saida do print""")
 
 
 
-if x > 0:   print("numero positivo")
-idade = 17
-if idade >= 18:
-   print("vc é maior de idade")
-#else: 
-   print("vc é menor de idade")
 
 
-####################################
+# ########################################################
+# print(f""" 14 . verifique se (a is b): {a is b}
+#             14 . verifique se (a is c) : {a is c}""")
 
-nota = 50
-if nota >= 75:
-    print("excelente")
-elif nota >=50:
-    print("na trinca")
-else:
-    print("reprovado")
 
+# print("o valor é:", a)
+# print("o valor de A é:",a, "e o valor de B é:", b)
+# print(f"o valor de A é: {a} e o alor de B é: {b}")
 
+# ##########################
 
-contador = 1
-while  contador <=5:
-    print("numero1", contador)
-    contador += 1
 
+# a = [1,2,3]
+# b = a
+# c = [1,2,3]
+# print (a is b)
+# print (a is c)
 
 
+# print("o valor é:", )
+# print("o valor de A é:",a, "e o valor de B é:", b)
+# print(f"o valor de A é: {a} e o alor de B é: {b}")
 
 
-for i in range(1, 6):
-    print("nuemro:", i)
 
+# numeros = [1,2,3,4,5]
+# print(6 in numeros)
 
 
+# numeros = [12,15,17,23,27]
+# print(17 in numeros)
 
+# ########################################################################
 
-for i in range(1, 10):
-    if i == 3:
-        continue  #pula o numero 3
-    if i == 5:
-        break     # para o loop no 5
-    print(i)
+# nome = input("digite seu nome:  ")
+# idade = int(input("digite sua idade:  "))
 
 
+# Idade_daqui_100 = print (100 - idade)
 
-    usuario = 23
-if usuario >=1:
-    print("maior que zero")
-elif usuario ==0:
-    print("zero")
-else:
-    print("menor que zero")
 
+# if idade >= 18:  print("vc é maioridade")
+# else: print("vc é menor de idade")
 
-nota = 100
-if nota >=90:
-    print("excelente")
-elif nota >=60:
-    print("aprovado")
-else: 
-    print("reprovado")
 
+# if 13 <= idade <=19: print("vc é adolescente")
 
-idade = 15
-if idade <=13:
-    print("criança")
-elif idade <=17:
-    print("adolescente")
-else:
-    print("adulto")
+# idade += 5 
+# print (f"sua idade daqui 5 anos sera de: {idade}")
 
+ 
+# print("vc tem a letra N no nome", 'n' in nome)
 
 
 
-    contador = 1
-#while  contador <=10:
-   # print("numero1", contador)
-    #contador += 1
+# if x > 0:   print("numero positivo")
+# idade = 17
+# if idade >= 18:
+#    print("vc é maior de idade")
+# #else: 
+#    print("vc é menor de idade")
 
-###################################################
 
-for i in range(2, 21):
-    #if i % 2 == 0:
-        #print(i)
+# ####################################
 
-############################################################
+# nota = 50
+# if nota >= 75:
+#     print("excelente")
+# elif nota >=50:
+#     print("na trinca")
+# else:
+#     print("reprovado")
 
-n1 = int(input("coloque seu nomero:"))
-n2 = int(input("coloque o numero do seu filho:"))
-n3 = int(input("coloque o numero da mae:"))
- n4 = int(input("coloque o numero do pai:"))
- n5 = int(input("coloque o numero do avô:"))
- soma = n1 + n2 + n3 + n4 +n5
- print("a soma das idades sao:", soma)
 
-############################################################################################
 
- for i in range (1, 11):
-     i = i * 7
-     print(i)
+# contador = 1
+# while  contador <=5:
+#     print("numero1", contador)
+#     contador += 1
 
-###############################################################################
 
- soma = 0
- for i in range(1, 10):
-     n1 = int(input("digite um numero:"))
-     if n1 == 0:
-      break
-     else:
-       soma = soma + n1
- print(soma)
 
-################################################
 
- contador = 10
- while  contador <=50:
-     print("numero1", contador)
-     contador -= 1
 
+# for i in range(1, 6):
+#     print("nuemro:", i)
 
 
-#########################################
 
 
- ex: L = [20, 'jessa', 35.75, [30, 60, 90]]
- print(L)
 
+# for i in range(1, 10):
+#     if i == 3:
+#         continue  #pula o numero 3
+#     if i == 5:
+#         break     # para o loop no 5
+#     print(i)
 
- frutas = ['maça',   'banana', 'laranja']
- numeros = list(range(5))
- print(frutas)
 
- frutas = ["maça", "banana", "laranja"]
- for fruta in frutas:
-     print(fruta)
 
+#     usuario = 23
+# if usuario >=1:
+#     print("maior que zero")
+# elif usuario ==0:
+#     print("zero")
+# else:
+#     print("menor que zero")
 
- frutas.append("melao")
 
- print(frutas)
+# nota = 100
+# if nota >=90:
+#     print("excelente")
+# elif nota >=60:
+#     print("aprovado")
+# else: 
+#     print("reprovado")
 
- lista = [1, 2]
 
- lista.extend([2, 3, 4 ,5])
- lista.remove(2)
+# idade = 15
+# if idade <=13:
+#     print("criança")
+# elif idade <=17:
+#     print("adolescente")
+# else:
+#     print("adulto")
 
- print(lista)
 
- lista.count(x) #quantas vez aparece na lista
 
 
- lista.sort(x)  #coloca a lista em ordem
+#     contador = 1
+# #while  contador <=10:
+#    # print("numero1", contador)
+#     #contador += 1
 
+# ###################################################
 
+# for i in range(2, 21):
+#     #if i % 2 == 0:
+#         #print(i)
 
+# ############################################################
 
- lista = [3, 1, 5]
- nova = sorted(lista)
- print(nova)
+# n1 = int(input("coloque seu nomero:"))
+# n2 = int(input("coloque o numero do seu filho:"))
+# n3 = int(input("coloque o numero da mae:"))
+#  n4 = int(input("coloque o numero do pai:"))
+#  n5 = int(input("coloque o numero do avô:"))
+#  soma = n1 + n2 + n3 + n4 +n5
+#  print("a soma das idades sao:", soma)
 
+# ############################################################################################
 
+#  for i in range (1, 11):
+#      i = i * 7
+#      print(i)
 
- lista.sort(reverse=True)
+# ###############################################################################
 
-###########################################
+#  soma = 0
+#  for i in range(1, 10):
+#      n1 = int(input("digite um numero:"))
+#      if n1 == 0:
+#       break
+#      else:
+#        soma = soma + n1
+#  print(soma)
+
+# ################################################
+
+#  contador = 10
+#  while  contador <=50:
+#      print("numero1", contador)
+#      contador -= 1
+
+
+
+# #########################################
+
+
+#  ex: L = [20, 'jessa', 35.75, [30, 60, 90]]
+#  print(L)
+
+
+#  frutas = ['maça',   'banana', 'laranja']
+#  numeros = list(range(5))
+#  print(frutas)
+
+#  frutas = ["maça", "banana", "laranja"]
+#  for fruta in frutas:
+#      print(fruta)
+
+
+#  frutas.append("melao")
+
+#  print(frutas)
+
+#  lista = [1, 2]
+
+#  lista.extend([2, 3, 4 ,5])
+#  lista.remove(2)
+
+#  print(lista)
+
+#  lista.count(x) #quantas vez aparece na lista
+
+
+#  lista.sort(x)  #coloca a lista em ordem
+
+
+
+
+#  lista = [3, 1, 5]
+#  nova = sorted(lista)
+#  print(nova)
+
+
+
+#  lista.sort(reverse=True):
+
+# ###########################################
+
+
+
+#############################################################################16/09
+# lista_compras = [] #array
+
+# while True: #enquanto for verdade(a escrita de baixo).
+#     item = input("digite um item para adicionar à lista(ou 'sair' para finalizar:)")#enquanto o user estiver colocando coisas na lista e nao escrever 'sair' ira continuar a lista.
+
+#     if item in item =="sair": #se o item que esta dentro do "item" estiver escrito 'sair' ele vai parar de pedir para o user digitar coisa para colocar na lista.
+#         break #parar/quebrar.
+
+#             #ou
+
+#    #if item.lower() =="sair"
+#        #break
+     
+#     lista_compras.append(item) #variavel para procurar "itens" na lista_compras do user.
+
+# print("sua lista de compras:") #mostrar na tela a lista_compras do user.
+# for i, produto in enumerate(lista_compras, start=1): # para listar numerando o produto da (lista_compras(1, 2 , 3 , 4 ,5...) mas posso colocar "start=(algum numero) para que ele comece a contagem apartir de qual numero eu quiser").
+#     print(f"{i}. {produto}") #mostrar o indice(numero(1, 2, 3, 4, 5,...)) . mostrar o produto enumerate"enumerado" que o user escreveu anteriormente.
