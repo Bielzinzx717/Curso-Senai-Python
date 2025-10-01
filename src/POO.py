d95230842170ebcf5cd86cb0aef46b93cac76792
gabriel <gabrielborges0111@gmail.com>
2025-09-30 21:46:37 -0300

    30/09

--- src/POO.py
+++ src/POO.py
@@ -26,7 +26,7 @@
 # Conta.mostrar_saldo()
 
 
-##########################################################################
+# #########################################################################
 
 
 # class ContaBancaria:
@@ -49,7 +49,7 @@
 # print(conta.get_saldo())
 
 
-###################################################################
+# ##################################################################
 
 
 # class animal:
@@ -78,7 +78,7 @@
 # print(f"{cat}")
 
 
-#########################################################################
+# ########################################################################
 
 
 # class Funcionario:
@@ -109,7 +109,7 @@
 # print(f"seu novo salario é de {salario.get_salario()}")
 
 
-###############################################################################
+# ##############################################################################
 
 
 # class Produto:
@@ -168,7 +168,236 @@
 
 
 
-###########################################################################
+# ##########################################################################
 
 
 
+# class Funcionario:
+#     def trabalhar(self):
+#         print("O funcionario está trabalhando normalmente")
+
+
+# class Estagiario(Funcionario):
+#     def trabalhar(self):
+#         print("O estagiario está aprendendo e ajudando nas tarefas")
+
+
+
+# f1 = Funcionario()
+# e1 = Estagiario()
+
+# f1.trabalhar()
+# e1.trabalhar()
+
+
+
+# ###################################################
+
+
+# class ContaCorrente:
+#     def __init__ (self, saldo):
+#         self.__saldo = saldo
+
+
+#     def depositar(self, valor):
+#         if valor > 0:
+#             self.__saldo += valor
+#             print(f"Deposito de R${valor} realizado com sucesso")
+#         else:
+#             print("Valor invalido.")
+
+    
+#     def sacar(self, valor):
+#         if valor <= 0:
+#             print("valor invalido")
+#         elif valor <= self.__saldo:
+#             self.__saldo -= valor
+#             print(f"seu saque foi concluido no valor deR$ {valor}")
+
+#         else:
+#             print("seu saque nao foi concluido por falta de money")
+
+
+#     def consultar_saldo(self):
+#         print(f"saldo atual R$ {self.__saldo}")
+
+
+# v1 = ContaCorrente(100)
+# v1.depositar(50)
+# v1.sacar(50)
+# v1.consultar_saldo()
+
+
+
+
+# ##############################
+
+
+
+
+# class Aluno:
+#     def __init__(self, nome, nota):
+#         self.__nome = nome
+#         self.__nota = nota
+
+#     def get_nota(self):
+#         return self.__nota
+    
+#     def set_nota(self):
+#         if self.__nota > 0:
+#             print("sua nota foi aceita")
+#         else:
+#             print("sua nota nao foi aceita")
+
+
+#     def nota_max(self):
+#         if self.__nota == 10:
+#             print("parabens vc tirou nota maxima")
+
+
+
+# n1 = Aluno("gabriel", 10)
+# n1.get_nota()
+# n1.set_nota()
+# n1.nota_max()
+        
+    
+
+# #####################################################
+
+
+# class Pessoa:
+#     def __init__(self, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
+
+    
+# class Prof(Pessoa):
+#     def __init__(self, nome, idade, materia):
+#         super().__init__(nome, idade)
+#         self.materia = materia
+
+# p1 = Pessoa()
+# f1 = Prof()
+
+
+
+
+
+# ########################################
+
+
+
+# class Veiculo:
+#     def __init__(self, nome):
+#         self.nome = nome
+    
+#     def mover(self):
+#         print("carro parado")
+
+
+# class Carro(Veiculo):
+#     def mover(self):
+#         print("carro esta andando")
+
+
+# class Bicicleta(Veiculo):
+#     def mover(self):
+#         print("A bike esta andando")
+
+
+
+# Carro = Carro("civic")
+# Bicicleta = Bicicleta("moto")
+# Carro.mover()
+# Bicicleta.mover()
+
+
+
+
+
+# ############################################
+
+
+# class Usuario:
+#     def __init__(self, nome, senha):
+#         self.nome = nome
+#         self.__senha = senha
+
+#     def alterar_senha(self, antiga, nova):
+#         if antiga == self.__senha:
+#             self.__senha = nova
+#             print("senha alterada com sucesso")
+
+#         else:
+#             print("senha antiga incorreta. Tente novamente")
+
+#     def login(self, senha):
+#         if senha == self.__senha:
+#             print(f"aecsso permitido. Bem-vindo {self.nome}")
+
+#         else:
+#             print("senha incorreta.")
+
+# usuario1 = Usuario("gabriel" , "1234")
+
+# usuario1.login("1234")
+
+# usuario1.alterar_senha("1234" , "123")
+
+# usuario1.login("123")
+
+# usuario1.login("123")
+
+
+
+# ##############################################################
+
+
+
+# class Animal:
+#     def mover(self):
+#         print("movendo")
+
+# class Mamifero(Animal):
+#     def amamentar(self):
+#         print("amamentou")
+
+# class Cachorro(Mamifero):
+#     def latir(self):
+#         print("latiu")
+
+
+# dog = Cachorro()
+
+# dog.mover()
+# dog.amamentar()
+# dog.latir()
+
+
+
+##########################################################
+
+
+
+from abc import ABC, abstractclassmethod
+
+class Instrumento(ABC):
+    @abstractclassmethod
+    def tocar(self):
+        pass
+
+class Violão(Instrumento):
+    def tocar(self):
+        print("O violão está ticando um som suave de cordas.")
+
+
+class Bateria(Instrumento):
+    def tocar(self):
+        print("a bateria está marcadoo ritimo com batidas fortes")
+
+violao = Violão()
+bater = Bateria()
+
+violao.tocar()
+bater.tocar()
