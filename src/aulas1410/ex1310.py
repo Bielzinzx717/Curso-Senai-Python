03b28c73ebb414003ecc4e66e4148238038965e1
gabriel <gabrielborges0111@gmail.com>
2025-10-13 21:44:55 -0300

    13/10

---
+++ src/aulas1410/ex1310.py
@@ -0,0 +1,48 @@
+# from flask import Flask  
+# from flask_sqlalchemy import SQLAlchemy
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tarefas.db'
+# db = SQLAlchemy(app)
+
+
+# class Task(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     titulo = db.Column(db.String(100), nullable=False)
+#     descricao = db.Column(db.String(200))
+#     concluida = db.Column(db.Boolean, default=False)
+
+
+#     def __repr__(self):
+#         return f'<taks {self.titulo}>'
+    
+# with app.app_context():
+#     db.create_all()
+    
+
+
+# with app.app_context():
+#     nova_tarefa = Task(titulo= "estudar Flask", descricao = "aprender sobre modelagem de dados ")
+#     db.session.add(nova_tarefa)
+#     db.session.commit()
+
+#     tarefas = Task.query.all()
+#     print(tarefas)
+
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    tarefas = [
+        {"titulo": "Estudas Flask", "concluida": True},
+        {"titulo": "Criar API", "concluida": False},
+    ]
+    return render_template ("index.html", tarefas=tarefas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/registro.py
+++ src/registro.py
@@ -220,31 +220,31 @@
 from flask import Flask
 import os
 
-app = Flask(__name__)
-@app.route("/")
-def home():
-    soma = 30 + 57
-    mult = 15 * 7
-    div = 25 / 4
-    resto = 37 % 5
-    potencia = 6 ** 4
+# app = Flask(__name__)
+# @app.route("/")
+# def home():
+#     soma = 30 + 57
+#     mult = 15 * 7
+#     div = 25 / 4
+#     resto = 37 % 5
+#     potencia = 6 ** 4
 
-    resposta = f"""
-    <h1> Operadores Aritmeticos<h1>
-    <ul>
-        <li> 1. calcule a soma de 30 + 57: {soma}</li>
-        <li> 2. multiplique 15 por 7: {mult}</li>
-        <li> 3. divida 25 por 4 e veja o resultado: {div}</li>
-        <li> 4. descubra o resto da divisao de 37 por 5: {resto}</li>
-        <li> 5. ele 6 a potencia de 4: {potencia}</li>
+#     resposta = f"""
+#     <h1> Operadores Aritmeticos<h1>
+#     <ul>
+#         <li> 1. calcule a soma de 30 + 57: {soma}</li>
+#         <li> 2. multiplique 15 por 7: {mult}</li>
+#         <li> 3. divida 25 por 4 e veja o resultado: {div}</li>
+#         <li> 4. descubra o resto da divisao de 37 por 5: {resto}</li>
+#         <li> 5. ele 6 a potencia de 4: {potencia}</li>
 
-    </ul>
-    """
+#     </ul>
+#     """
 
-    return resposta
+#     return resposta
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 
